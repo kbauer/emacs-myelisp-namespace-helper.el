7bfc76dde5cceece1315370495ebe50af8e9de1f
Klaus-Dieter Bauer <kbauer@users.noreply.github.com>
2018-06-14 22:51:42 +0200

    Created proper repository with separate `README.md` file from gist.
    Added line that properly activates global mode if enabled in the
    customization interface.

---
+++ README.md
@@ -0,0 +1,128 @@
+<!-- -*-coding: utf-8 --* -->
+
+MOTIVATION
+==========
+
+In Emacs Lisp (elisp), there is no native namespace system. While it
+has the convenient consequence, that the same symbol means the same
+everywhere (a boon for interactive features like `M-x
+describe-function` or `M-x find-function`), it also results in a lot
+of “boilerplate” re-typing of namespace prefix strings.
+
+While implementations of macro-based namespaces such as `names.el`
+exist, they are bound to lack support for current or future
+interactive features, that depend on the presence of full function
+names in the source-code files.
+
+Any attempt to implement namespaces as macros is bound
+to miss some current or future interactive feature
+depending on full function names in the code.
+
+This provisional package is meant to make working with namespace
+prefices more convenient in the meantime.
+
+It is also meant for experimenting with what works.
+
+
+
+USAGE
+=====
+
+  - Download `myelisp-namespace-helper.el`.
+  
+  - Either
+  
+      * Copy/paste the contents into your `.emacs` file, or
+        (preferably)
+      * Put a statement
+      
+            (load-file "PATH/TO/myelisp-namespace-helper.el")
+        
+        into your `.emacs` file.
+  
+  - Enable `M-x myelisp-namespace-helper-mode` in buffers, where you
+    want to use it. Optionally, enable `M-x
+    myelisp-namespace-helper-global-mode`.
+  
+  - In `emacs-lisp-mode` or `lisp-interaction-mode`, insert a
+    buffer-appropriate namespace prefix by pressing `-` once, while
+    not already inside a symbol, and again for a private prefix
+    according to the `NAMESPACE-public-name` and
+    `NAMESPACE--private-name` covention.
+    
+    The `-`-key doesn't insert a prefix
+    
+      * when typed after symbol characters,
+      * when immediately followed by a number or whitespace,
+      * when typed inside a string or comment, unless preceded by a
+        backtick (`` ` ``) or quote (`'`) character, or
+      * when inserted using `quoted-insert` (normally `C-q -`).
+    
+With this code, `prettify-symbols-mode` collapses the namespace
+prefix into a highlighted hyphen, mirroring the input method.
+
+
+
+CHANGELOG
+=========
+
+**2018-06-14**
+  - Created proper repository with separate `README.md` file from gist.
+  - Added line that properly activates global mode if enabled
+    in the customization interface.
+
+**2018-06-12** Rewrote in terms of the `post-self-insert-hook`. Sadly,
+on the way the limited support for the `eval-expression` as only case
+of minibuffer usage was lost too.
+
+The improvement, that now `-1` or `(- 1 2)` can be typed without
+producing a prefix, is more important however.
+
+Also merged CHANGELOG into the source file.
+
+**2018-06-10** Bugfix: Limited the faces that affect the `-` key;
+Previously `show-paren-match` face prevented insertion when cursor
+located immediately before a `(`.
+
+**2018-06-08** More documentation.
+
+**2018-06-07** Made file namespace-clean.
+
+
+
+TODO: IMPROVEMENTS?
+===================
+
+  - Prettification, and maybe insertion, should possibly be limited to
+    `private--names` by default. The programmer should remain
+    aware of how verbose client code will see the “public”
+    interface.
+
+  - This would also solve the issue of typing negative numbers
+    or the `-` function without `C-q` prefix.
+    
+  - Need to find way to support minibuffer. I currently have no
+    general way of telling generally, if the read-request to the
+    minibuffer wants a symbol/expression as completion.
+
+
+WHAT DID NOT WORK
+=================
+
+  - Key `~`. It is the least common printable ASCII character
+    in Emacs's lisp code, but using the same key for inserting
+    the prefix and for separating words in symbols proved a huge
+    usability advantage.
+
+  - Double `--` for prefix, triple `---` for private prefix.
+    Possibly a matter of training, but it felt wrong.
+
+
+TODO: FUTURE PLANS
+==================
+
+  - Separate minor mode from `prettify-symbols-mode`.
+
+  - Make it configurable, and cross-mode, as a minor-mode. E.g.
+    in LaTeX, binding @@ to produce a package-appropriate prefix
+    would be useful.
--- myelisp-namespace-helper.el
+++ myelisp-namespace-helper.el
@@ -22,7 +22,8 @@
 
 ;;;; TEMPORARY PREFIX HELPERS
 ;; Until I implement a proper mode.
-;; Published as `https://gist.github.com/kbauer/f629eaf78f8dd1117225eaf2baf717d5'
+;; Currently published as `https://github.com/kbauer/myelisp-namespace-helper.el'
+;; Previously published as `https://gist.github.com/kbauer/f629eaf78f8dd1117225eaf2baf717d5'
 ;; 
 ;; See docstring of `myelisp-prefix-helper'.
 
@@ -89,123 +90,7 @@ See `myelisp-namespace-helper-function' for details.")
 
 
 (define-minor-mode myelisp-namespace-helper-mode 
-  "Use the `-' key to insert a namespace prefix.
-
-
-USAGE
-=====
-
-  - Paste into your .emacs file
-  
-  - In emacs-lisp or lisp-interaction modes, 
-    insert a buffer-appropriate namespace prefix 
-    by pressing `-' once, while not already inside a symbol.
-    
-      * Following an already started symbol name, just
-        behave like normal `self-insert-command'.
-      
-      * The same applies inside strings and comments,
-        unless `point' is preceded by a backquote (`)
-        or quote (').
-      
-      * When invoked with a prefix argument, likewise
-        delegate to `self-insert-command'.
-    
-  - Used `quoted-insert' (normally `C-q -') to insert
-    literal hypens.
-
-  - Works for `M-x eval-expression.' too. Here the
-    last used emacs-lisp buffer is assumed.
-
-  - Use file-variable `myelisp-namespace-helper-prefix'
-    to override default.
-  
-With this code, `prettify-symbols-mode' collapses the namespace
-prefix into a highlighted hyphen, mirroring the input method.
-
-
-MOTIVATION
-==========
-
-In emacs-lisp, there is no native namespace system. While it has
-the convenient consequence, that the same symbol means the same
-everywhere (a boon for interactive features like `M-x
-describe-function' or `M-x find-function'), it also results in a
-lot of “boilerplate” re-typing of namespace prefix strings.
-
-While implementations of macro-based namespaces such as
-`names.el' exist, they are bound to lack support for current or
-future interactive features, that depend on the presence of full
-function names in the source-code files.
-
-Any attempt to implement namespaces as macros is bound
-to miss some current or future interactive feature
-depending on full function names in the code.
-
-This provisional package is meant to make working with namespace
-prefices more convenient in the meantime.
-
-It is also meant for experimenting with what works.
-
-
-CHANGELOG
-=========
-
-2018-06-12 Rewrote in terms of the `post-self-insert-hook'. Sadly,
-           on the way the limited support for the `eval-expression'
-           as only case of minibuffer usage was lost too.
-
-           The improvement, that now `-1' or `(- 1 2)' can be
-           typed without producing a prefix, is more important
-           however.
-           
-           Also merged CHANGELOG into the source file.
-
-2018-06-10 Bugfix: Limited the faces that affect the `-' key;
-           Previously `show-paren-match' face prevented insertion
-           when cursor located immediately before a `('.
-
-2018-06-08 More documentation.
-
-2018-06-07 Made file namespace-clean.
-
-
-TODO: IMPROVEMENTS?
-===================
-
-  - Prettification, and maybe insertion, should possibly be limited to
-    `private--names' by default. The programmer should remain
-    aware of how verbose client code will see the “public”
-    interface.
-
-  - This would also solve the issue of typing negative numbers
-    or the `-' function without `C-q' prefix.
-    
-  - Need to find way to support minibuffer. I currently have no
-    general way of telling generally, if the read-request to the
-    minibuffer wants a symbol/expression as completion.
-
-
-WHAT DID NOT WORK
-=================
-
-  - Key `~'. It is the least common printable ASCII character
-    in Emacs's lisp code, but using the same key for inserting
-    the prefix and for separating words in symbols proved a huge
-    usability advantage.
-
-  - Double `--' for prefix, triple `---' for private prefix.
-    Possibly a matter of training, but it felt wrong.
-
-
-TODO: FUTURE PLANS
-==================
-
-  - Separate minor mode from `prettify-symbols-mode'.
-
-  - Make it configurable, and cross-mode, as a minor-mode. E.g.
-    in LaTeX, binding @@ to produce a package-appropriate prefix
-    would be useful."
+  "Use the `-' key to insert a namespace prefix in `emacs-lisp-mode'."
   :lighter " Nsh"
   
   (when myelisp-namespace-helper-mode
@@ -219,6 +104,9 @@ TODO: FUTURE PLANS
     (cond
       ((memq major-mode myelisp-namespace-helper-affected-modes)
        (myelisp-namespace-helper-mode +1)))))
+
+(when myelisp-namespace-helper-global-mode
+  (myelisp-namespace-helper-global-mode +1))
 
 
 (defun myelisp-namespace-helper-get-buffer-namespace (&optional buf)
